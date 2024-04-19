2c2931f521152b3184a8ff4b950364a96b0903a3
4TEAM <alqasos@icloud.com>
2024-04-20 03:38:36 +0500

    MESS

--- src/components/MainFoot.js
+++ src/components/MainFoot.js
@@ -3,42 +3,42 @@ import styles from "./FrameComponent.module.css";
 const MainHeader = () => {
 
   return (
-    <section className={styles.mainInner}>
-      <div className={styles.frameParent}>
-        <div className={"energyBar"}>
-          <b className={"energyLimit"}>🔋 1500/1500</b>
+      <section className={styles.mainInner}>
+        <div className={styles.frameParent}>
+          <div className={styles.energyuWrapper}>
+            <b className={styles.energyu}>🔋 1500/1500</b>
+          </div>
+          <div className={styles.menuParent}>
+            <div className={styles.menu} />
+            <div className={styles.frameGroup}>
+              <div className={styles.iconUserFriendsWrapper}>
+                <img
+                    className={styles.iconUserFriends}
+                    alt=""
+                    src="/-icon-user-friends.svg"
+                />
+              </div>
+              <b className={styles.friends}>Friends</b>
+            </div>
+            <div className={styles.frameContainer}>
+              <div className={styles.iconCoinsWrapper}>
+                <img className={styles.iconCoins} alt="" src="/-icon-coins.svg" />
+              </div>
+              <b className={styles.missions}>Missions</b>
+            </div>
+            <div className={styles.frameDiv}>
+              <div className={styles.iconFlash1Wrapper}>
+                <img
+                    className={styles.iconFlash1}
+                    alt=""
+                    src="/-icon-flash-1.svg"
+                />
+              </div>
+              <b className={styles.boost}>Boost</b>
+            </div>
+          </div>
         </div>
-        <div className={"menuBar"}>
-          <div className={styles.menu} />
-          <div className={"friendsButton"}>
-            <div className={styles.iconUserFriendsWrapper}>
-              <img
-                className={styles.iconUserFriends}
-                alt=""
-                src="/-icon-user-friends.svg"
-              />
-            </div>
-            <b className={styles.friends}>Friends</b>
-          </div>
-          <div className={"missionsButton"}>
-            <div className={styles.iconCoinsWrapper}>
-              <img className={styles.iconCoins} alt="" src="/-icon-coins.svg" />
-            </div>
-            <b className={styles.missions}>Missions</b>
-          </div>
-          <div className={"boostButton"}>
-            <div className={styles.iconFlash1Wrapper}>
-              <img
-                className={styles.iconFlash1}
-                alt=""
-                src="/-icon-flash-1.svg"
-              />
-            </div>
-            <b className={styles.boost}>Boost</b>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
   );
 };
 
