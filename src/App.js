ec506312e5d26c1a47c9b68d7ca3ae3200a64117
4TEAM <alqasos@icloud.com>
2024-04-19 18:53:26 +0500

    test ui

--- src/App.js
+++ src/App.js
@@ -9,10 +9,42 @@ function App() {
         tg.close();
     }
   return (
-    <div className="App">
-      YOUR NAME: {tg.initDataUnsafe?.user?.first_name}
-        <button onClick={onClose}>Close</button>
-    </div>
+      <div className="main">
+          <section className="main-inner">
+              <div className="profile-parent">
+                  <h2 className="profile">👤 Sarvar</h2>
+                  <div className="question-mark-icon">
+                      <img
+                          className="icon-so-faq"
+                          loading="lazy"
+                          alt=""
+                          src="/-icon-so-faq.svg"
+                      />
+                  </div>
+              </div>
+          </section>
+          <div className="mines-wrapper">
+              <h2 className="mines">Your True Coins here:</h2>
+          </div>
+          <section className="mines-container">
+              <div className="mines1">1 507 999</div>
+          </section>
+          <section className="main-child">
+              <div className="frame-parent">
+                  <img
+                      className="frame-child"
+                      loading="lazy"
+                      alt=""
+                      src="/group-1.svg"
+                  />
+                  <h1 className="t">T</h1>
+              </div>
+          </section>
+          <div className="energy-u-i">
+              <b className="energyu">🔋 1500/1500</b>
+          </div>
+          <FrameComponent />
+      </div>
   );
 }
 
