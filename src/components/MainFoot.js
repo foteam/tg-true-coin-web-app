9abe0804aba20f4bbead6082d4c57e5521f61866
4TEAM <alqasos@icloud.com>
2024-04-20 03:26:14 +0500

    MM

--- src/components/MainFoot.js
+++ src/components/MainFoot.js
@@ -5,12 +5,12 @@ const MainHeader = () => {
   return (
     <section className={styles.mainInner}>
       <div className={styles.frameParent}>
-        <div className={styles.energyuWrapper}>
-          <b className={styles.energyu}>🔋 1500/1500</b>
+        <div className={"energyBar"}>
+          <b className={"energyLimit"}>🔋 1500/1500</b>
         </div>
-        <div className={styles.menuParent}>
+        <div className={"menuBar"}>
           <div className={styles.menu} />
-          <div className={styles.frameGroup}>
+          <div className={"friendsButton"}>
             <div className={styles.iconUserFriendsWrapper}>
               <img
                 className={styles.iconUserFriends}
@@ -20,13 +20,13 @@ const MainHeader = () => {
             </div>
             <b className={styles.friends}>Friends</b>
           </div>
-          <div className={styles.frameContainer}>
+          <div className={"missionsButton"}>
             <div className={styles.iconCoinsWrapper}>
               <img className={styles.iconCoins} alt="" src="/-icon-coins.svg" />
             </div>
             <b className={styles.missions}>Missions</b>
           </div>
-          <div className={styles.frameDiv}>
+          <div className={"boostButton"}>
             <div className={styles.iconFlash1Wrapper}>
               <img
                 className={styles.iconFlash1}
