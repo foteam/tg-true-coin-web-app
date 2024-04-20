19b717a1c559d394d7fc482037afb1534ffff58c
4TEAM <alqasos@icloud.com>
2024-04-20 16:53:43 +0500

    123

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ function App() {
   const location = useLocation();
   const pathname = location.pathname;
   useEffect(() => {
-    tg.setHeaderColor('#ff000000')
     tg.expand();
-  })
+  }, [])
   useEffect(() => {
     if (action !== "POP") {
       window.scrollTo(0, 0);
